11a0561f8fa644014d767ffb4b009052ebe5b100
Cherono-mercy <cheronom142@gmail.com>
2024-01-20 22:56:10 +0800

    completed app component

---
+++ pizza_restaurants/src/components/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./Home";
+import Navbar from "./Navbar";
+import Restaurant from "./Restaurant";
+
+function App() {
+  return (
+    <Router>
+      <>
+        <Navbar />
+        <Routes>
+          <Route path="/restaurants/:id" element={<Restaurant />} />
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </>
+    </Router>
+  );
+}
+
+export default App;
